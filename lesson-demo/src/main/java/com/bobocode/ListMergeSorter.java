57772b6aa5efd990b00862f90384bb12f1a91dfc
Viktor Lyakhovych <lyakhovych.viktor@gmail.com>
2022-05-23 02:03:57 +0300

    some minor refactoring

--- lesson-demo/src/main/java/com/bobocode/ListMergeSorter.java
+++ lesson-demo/src/main/java/com/bobocode/ListMergeSorter.java
@@ -1,15 +1,16 @@
 package com.bobocode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ListMergeSorter {
 
     public static void main(String[] args) {
-        List<Integer> intList = List.of(9, 2, 7, 6, 8, 1);
+//        List<Integer> intList = List.of(9, 2, 7, 6, 8, 1); //Returns an unmodifiable(!!!) list
+        List<Integer> intList = Arrays.asList(9, 2, 7, 6, 8, 1);
         System.out.println(mergeSort(intList));
     }
-
 
     public static <T extends Comparable<T>> List<T> mergeSort(List<T> listToSort) {
 
@@ -18,29 +19,30 @@ public class ListMergeSorter {
         List<T> list01 = new ArrayList<>(listToSort.subList(0, listToSort.size() / 2));
         List<T> list02 = new ArrayList<>(listToSort.subList(listToSort.size() / 2, listToSort.size()));
 
-        list01 = new ArrayList<>(mergeSort(list01));
-        list02 = new ArrayList<>(mergeSort(list02));
+        mergeSort(list01);
+        mergeSort(list02);
+        mergeLists(listToSort, list01, list02);
+        return listToSort;
+    }
 
-        List<T> generalList = new ArrayList<>(listToSort); //to have it mutable
-
+    private static <T extends Comparable<T>> void mergeLists(List<T> listToSort, List<T> list01, List<T> list02) {
         int index01 = 0;
         int index02 = 0;
         for (int i = 0; i < list01.size() + list02.size(); i++) {
             if (index01 == list01.size()) {
-                generalList.set(i, list02.get(index02));
+                listToSort.set(i, list02.get(index02));
                 index02++;
             } else if (index02 == list02.size()) {
-                generalList.set(i, list01.get(index01));
+                listToSort.set(i, list01.get(index01));
                 index01++;
             } else if (list01.get(index01).compareTo(list02.get(index02)) < 0) {
-                generalList.set(i, list01.get(index01));
+                listToSort.set(i, list01.get(index01));
                 index01++;
             } else {
-                generalList.set(i, list02.get(index02));
+                listToSort.set(i, list02.get(index02));
                 index02++;
             }
         }
-        return generalList;
     }
 }
 
